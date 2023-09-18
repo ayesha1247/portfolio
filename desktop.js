0dac9c42385c37232021f0b6df33fee3bdadaac4
ayesha1247 <ayeshanazirch@gmail.com>
2023-09-18 15:11:58 +0500

    m

---
+++ desktop.js
@@ -0,0 +1,15 @@
+let twiiterButton = document.getElementById("twitterIcon");
+twitterIcon.addEventListener("click", function() {
+    // Replace 'your_twitter_username' with your actual Twitter username or the URL of your Twitter profile
+    let twitterUrl = "https://twitter.com/Ayeshannappps";
+  
+    // Open a new window or tab with your Twitter page
+    window.open(twitterUrl, "_blank");
+  });
+  let linkedInButton = document.getElementById("linkedInIcon");
+  linkedInIcon.addEventListener("click", function() {
+      let linkedInUrl = "https://www.linkedin.com/in/ayesha-ch-a81484157/";
+    
+      window.open(linkedInUrl, "_blank");
+    });
+  
